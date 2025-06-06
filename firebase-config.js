a4e7623cf719f54e106ffcf80ac41a019f6998f1
willianpesenti <62893140+willianpesenti@users.noreply.github.com>
2025-06-06 15:20:02 -0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBGAt4hPAai7y-Q4BCXrynQMV3uzYzpFuQ",
+  authDomain: "pontuacao-jogadores.firebaseapp.com",
+  databaseURL: "https://pontuacao-jogadores-default-rtdb.firebaseio.com",
+  projectId: "pontuacao-jogadores",
+  storageBucket: "pontuacao-jogadores.appspot.com",
+  messagingSenderId: "838596739133",
+  appId: "1:838596739133:web:b88ecacd10d6d2c7016a54"
+};
